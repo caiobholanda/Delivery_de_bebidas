1405c334903f14f2d1f13f1d261858c18bc317e4
caiobholanda <caiobholanda2007@gmail.com>
2026-09-16 22:28:43 -0300

    refactor(repositorio): CupomRepository implementa Leitura e Escrita

--- src/main/java/com/delivery/repositorio/CupomRepository.java
+++ src/main/java/com/delivery/repositorio/CupomRepository.java
@@ -1,6 +1,21 @@
 package com.delivery.repositorio;
 
+import java.util.List;
 import com.delivery.dominio.*;
-public interface CupomRepository extends Leitura<Cupom>, Escrita<Cupom> {
-    Cupom buscarPorCodigo(String codigo);
+public class CupomRepository implements Leitura<Cupom>, Escrita<Cupom> {
+    public Cupom buscarPorCodigo(String codigo) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Cupom buscarPorId(int id) {
+        throw new UnsupportedOperationException();
+    }
+    public List<Cupom> listar() {
+        throw new UnsupportedOperationException();
+    }
+    public Cupom salvar(Cupom entidade) {
+        throw new UnsupportedOperationException();
+    }
+    public void remover(int id) {
+    }
 }
