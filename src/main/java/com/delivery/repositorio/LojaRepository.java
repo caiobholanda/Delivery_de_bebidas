c9b92a450cbbeae2c11fd251b2fadc92e337b3ba
caiobholanda <caiobholanda2007@gmail.com>
2026-09-16 22:28:43 -0300

    refactor(repositorio): LojaRepository implementa Leitura e Escrita

--- src/main/java/com/delivery/repositorio/LojaRepository.java
+++ src/main/java/com/delivery/repositorio/LojaRepository.java
@@ -1,6 +1,21 @@
 package com.delivery.repositorio;
 
+import java.util.List;
 import com.delivery.dominio.*;
-public interface LojaRepository extends Leitura<Loja>, Escrita<Loja> {
-    Loja buscarPorCnpj(String cnpj);
+public class LojaRepository implements Leitura<Loja>, Escrita<Loja> {
+    public Loja buscarPorCnpj(String cnpj) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Loja buscarPorId(int id) {
+        throw new UnsupportedOperationException();
+    }
+    public List<Loja> listar() {
+        throw new UnsupportedOperationException();
+    }
+    public Loja salvar(Loja entidade) {
+        throw new UnsupportedOperationException();
+    }
+    public void remover(int id) {
+    }
 }
