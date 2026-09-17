4fa96e0946bdfadb8afec2ea63969150ddb16808
caiobholanda <caiobholanda2007@gmail.com>
2026-09-16 22:28:43 -0300

    refactor(repositorio): EntregaRepository implementa Leitura e Escrita

--- src/main/java/com/delivery/repositorio/EntregaRepository.java
+++ src/main/java/com/delivery/repositorio/EntregaRepository.java
@@ -2,7 +2,23 @@ package com.delivery.repositorio;
 
 import java.util.List;
 import com.delivery.dominio.*;
-public interface EntregaRepository extends Leitura<Entrega>, Escrita<Entrega> {
-    List<Entrega> listarPorEntregador(Entregador entregador);
-    int contarAtivasPorEntregador(Entregador entregador);
+public class EntregaRepository implements Leitura<Entrega>, Escrita<Entrega> {
+    public List<Entrega> listarPorEntregador(Entregador entregador) {
+        throw new UnsupportedOperationException();
+    }
+    public int contarAtivasPorEntregador(Entregador entregador) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Entrega buscarPorId(int id) {
+        throw new UnsupportedOperationException();
+    }
+    public List<Entrega> listar() {
+        throw new UnsupportedOperationException();
+    }
+    public Entrega salvar(Entrega entidade) {
+        throw new UnsupportedOperationException();
+    }
+    public void remover(int id) {
+    }
 }
