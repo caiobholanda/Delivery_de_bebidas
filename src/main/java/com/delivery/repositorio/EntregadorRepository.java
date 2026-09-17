1d67f106f80fcf50b1a178f89f6f62e849ae667f
caiobholanda <caiobholanda2007@gmail.com>
2026-09-16 22:28:43 -0300

    refactor(repositorio): EntregadorRepository implementa Leitura e Escrita

--- src/main/java/com/delivery/repositorio/EntregadorRepository.java
+++ src/main/java/com/delivery/repositorio/EntregadorRepository.java
@@ -2,6 +2,20 @@ package com.delivery.repositorio;
 
 import java.util.List;
 import com.delivery.dominio.*;
-public interface EntregadorRepository extends Leitura<Entregador>, Escrita<Entregador> {
-    List<Entregador> listarDisponiveis();
+public class EntregadorRepository implements Leitura<Entregador>, Escrita<Entregador> {
+    public List<Entregador> listarDisponiveis() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Entregador buscarPorId(int id) {
+        throw new UnsupportedOperationException();
+    }
+    public List<Entregador> listar() {
+        throw new UnsupportedOperationException();
+    }
+    public Entregador salvar(Entregador entidade) {
+        throw new UnsupportedOperationException();
+    }
+    public void remover(int id) {
+    }
 }
