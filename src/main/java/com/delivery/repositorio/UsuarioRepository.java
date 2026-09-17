f931757a295cd77e031b7046a541a24e1d935423
caiobholanda <caiobholanda2007@gmail.com>
2026-09-16 22:28:43 -0300

    refactor(repositorio): UsuarioRepository implementa Leitura e Escrita

--- src/main/java/com/delivery/repositorio/UsuarioRepository.java
+++ src/main/java/com/delivery/repositorio/UsuarioRepository.java
@@ -1,6 +1,21 @@
 package com.delivery.repositorio;
 
+import java.util.List;
 import com.delivery.dominio.*;
-public interface UsuarioRepository extends Leitura<Usuario>, Escrita<Usuario> {
-    Usuario buscarPorEmail(String email);
+public class UsuarioRepository implements Leitura<Usuario>, Escrita<Usuario> {
+    public Usuario buscarPorEmail(String email) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Usuario buscarPorId(int id) {
+        throw new UnsupportedOperationException();
+    }
+    public List<Usuario> listar() {
+        throw new UnsupportedOperationException();
+    }
+    public Usuario salvar(Usuario entidade) {
+        throw new UnsupportedOperationException();
+    }
+    public void remover(int id) {
+    }
 }
